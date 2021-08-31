1ebf9b255d01e55183ecfdb118849ac22d311094
김기환 <khkim@allbigdat.com>
2021-08-31 11:54:28 +0900

    add spm and edit pod path

---
+++ Example/Pods/Target Support Files/SwizzleTest/SwizzleTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwizzleTest : NSObject
+@end
+@implementation PodsDummy_SwizzleTest
+@end
---
+++ Tests/SwizzleTestTests/SwizzleTestTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import SwizzleTest
+
+    final class SwizzleTestTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(SwizzleTest().text, "Hello, World!")
+        }
+    }
